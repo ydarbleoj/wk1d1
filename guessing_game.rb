e13a5f377ac64984e743ffcd02b979b714918211
ydarbleoj <brady.joel@gmail.com>
2014-01-07 11:42:01 -0800

    indented if statement

--- guessing_game.rb
+++ guessing_game.rb
@@ -8,14 +8,14 @@ guesses = 1
  
 
 until guess == number do 
-if guess < number 
-  p "The number is higher than #{guess}. Guess again"
-elsif guess > number 
-  p "The number is lower than #{guess}. Guess again"
-end 
+  if guess < number 
+    p "The number is higher than #{guess}. Guess again"
+  elsif guess > number 
+    p "The number is lower than #{guess}. Guess again"
+  end 
 
-guess = gets.to_i
-guesses += 1
+  guess = gets.to_i
+  guesses += 1
 end 
 
 puts "You got it in #{guesses} tries"
