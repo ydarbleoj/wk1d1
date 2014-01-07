39a2ea6db5ea02b66f54cc2903eb8eebb66c3e56
ydarbleoj <brady.joel@gmail.com>
2014-01-07 11:45:17 -0800

    removed comments

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,12 +1,8 @@
-# Guessing game 
-# this another comment 
-
 number = rand(1..100)
 puts "Guess a number between 1 and 100"
 guess = gets.to_i
 guesses = 1
  
-
 until guess == number do 
   if guess < number 
     p "The number is higher than #{guess}. Guess again"
